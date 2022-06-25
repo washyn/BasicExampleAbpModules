8df946bc28bb875bce1cbf8e9a6b375219fc2e37
Washington Acero M <shin321654987@gmail.com>
2022-06-25 14:58:17 -0700

    aded chart bars

--- WebApplicationMvc/Controllers/HomeController.cs
+++ WebApplicationMvc/Controllers/HomeController.cs
@@ -3,10 +3,15 @@ using Microsoft.Extensions.Logging;
 using System;
 using System.Collections.Generic;
 using System.Diagnostics;
+using System.Globalization;
 using System.Linq;
+using System.Linq.Dynamic.Core;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Authorization;
+using Microsoft.EntityFrameworkCore;
+using WebApplicationMvc.EfCore;
 using WebApplicationMvc.Models;
+using WebApplicationMvc.ViewModels;
 
 namespace WebApplicationMvc.Controllers
 {
@@ -14,10 +19,12 @@ namespace WebApplicationMvc.Controllers
     public class HomeController : Controller
     {
         private readonly ILogger<HomeController> _logger;
+        private readonly ApplicationDbContex _dbContex;
 
-        public HomeController(ILogger<HomeController> logger)
+        public HomeController(ILogger<HomeController> logger, ApplicationDbContex dbContex)
         {
             _logger = logger;
+            _dbContex = dbContex;
         }
 
         public IActionResult Index()
@@ -40,8 +47,61 @@ namespace WebApplicationMvc.Controllers
         [Authorize(Roles = Rol.Asistente + "," + Rol.Medico)]
         public IActionResult Dashboard()
         {
-            return View();
+            var model = _dbContex.Citas
+                .Include(a => a.UsuarioPaciente)
+                .Where(a => a.Estado == EstadoCita.Atendido)
+                .OrderByDescending(a => a.FechaHora)
+                .Select(a => new PacientesRecientesViewModel()
+                {
+                    Categoria = a.Categoria,
+                    Nombres = a.UsuarioPaciente.ToString(),
+                    FechaHora = a.FechaHora
+                })
+                .Take(10)
+                .ToList();
+            return View(model);
         }
 
+        public IActionResult TipoAtencion()
+        {
+            var model = _dbContex.Citas
+                .GroupBy(a => a.Categoria)
+                .Select(a => new DonutChartViewModel()
+                {
+                    Label = a.Key,
+                    Value = a.Count(),
+                })
+                .ToList();
+            return Json(model);
+        }
+        
+        public IActionResult AtencionesMensual()
+        {
+            var culture = new CultureInfo("es-cl");
+            var date = DateTime.Now;
+            var dat1 = date.AddMonths(1);
+            var junio = culture.DateTimeFormat.GetMonthName(date.Month);
+            var julio = culture.DateTimeFormat.GetMonthName(dat1.Month);
+            
+            var modeljunio = _dbContex.Citas
+                .Count(a => a.FechaHora.Year == date.Date.Year && a.FechaHora.Month == date.Date.Month);
+            
+            var modeljulio = _dbContex.Citas
+                .Count(a => a.FechaHora.Year == dat1.Date.Year && a.FechaHora.Month == dat1.Date.Month);
+
+            return Json(new List<BarChart>()
+            {
+                new BarChart()
+                {
+                    Month = junio,
+                    Data = modeljunio,
+                },
+                new BarChart()
+                {
+                    Month = julio,
+                    Data = modeljulio
+                }
+            });
+        }
     }
 }
---
+++ WebApplicationMvc/ViewModels/PacientesRecientesViewModel.cs
@@ -0,0 +1,11 @@
+﻿using System;
+
+namespace WebApplicationMvc.ViewModels
+{
+    public class PacientesRecientesViewModel
+    {
+        public string Nombres { get; set; }
+        public string Categoria { get; set; }
+        public DateTime FechaHora { get; set; }
+    }
+}
